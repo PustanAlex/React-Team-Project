051a0a15393bf45e724a1329dc2986a094a73d3c
PustanAlex <alecsandru_bogdy@yahoo.com>
2024-06-30 20:01:26 +0300

    Revert "Merge pull request #16 from PustanAlex/pustan-alex"
    This reverts commit 5d02b8779972eca11174f7c39ea3564390572063, reversing
    changes made to c661a02aa6088a21ae0233f98ebcdc8f52218843.

--- src/components/redux/fetchOperations/fetchOperations.js
+++ src/components/redux/fetchOperations/fetchOperations.js
@@ -2,6 +2,7 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
 export const fetchOperations = createAsyncThunk(
+<<<<<<< HEAD
   'transactions/fetchAll',
   async (_, thunkAPI) => {
     try {
@@ -9,6 +10,17 @@ export const fetchOperations = createAsyncThunk(
       return response.data;
     } catch (error) {
       return thunkAPI.rejectWithValue(error.message);
+=======
+// TODO: add fetch
+    'transtactions/fetchAll',
+    async(_, thunkAPI) => {
+        try {
+            const response = await axios.get('/transactions');
+            return response.data;
+        } catch (error) {
+            return thunkAPI.rejectwithValue(error.message);
+        }
+>>>>>>> parent of 5d02b87 (Merge pull request #16 from PustanAlex/pustan-alex)
     }
   }
 );
