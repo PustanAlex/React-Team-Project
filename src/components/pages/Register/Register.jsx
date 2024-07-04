e7bd981b79ab83a52f0ed2c8f7aec20402440a83
PustanAlex <alecsandru_bogdy@yahoo.com>
2024-07-04 22:33:17 +0300

    deploy

--- src/components/pages/Register/Register.jsx
+++ src/components/pages/Register/Register.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { useFormik } from 'formik';
 import * as Yup from 'yup';
 import { Link, useNavigate } from 'react-router-dom';
